142daf5b1e3b00465e2cb6cfe7b6b0d580882d30
NAITIK GUPTA <naitikgupta2713@gmail.com>
2026-09-19 21:02:48 +0530

    checking url remote

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,6 @@ export default function RootLayout({ children }) {
         <div className="absolute top-0 left-0 w-full z-50">
           <Navbar />
         </div>
-
         {/* Main page starts from the very top */}
         <main className="relative z-0 min-h-screen">
           {children}
